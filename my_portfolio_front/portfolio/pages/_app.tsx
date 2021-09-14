4c01a58d7588d928497879d43fc3e4910cd726fd
mtmtmshr <s17t274@stu.kagawa-u.ac.jp>
2021-09-14 20:28:48 +0900

    スクロールバーを常に表示させる

--- my_portfolio_front/portfolio/pages/_app.tsx
+++ my_portfolio_front/portfolio/pages/_app.tsx
@@ -1,8 +1,10 @@
 import { AppProps } from 'next/app'
 import '../styles/globals.css'
-
+import {useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+    // スクロールバーを常に表示
+    useEffect( () => { document.querySelector("body").classList.add("overflow-y-scroll") });
     return <Component {...pageProps} />
 }
 
