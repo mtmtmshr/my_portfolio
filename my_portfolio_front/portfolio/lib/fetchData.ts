23a37ba84159280073fc7300adc37da8eff19b59
mtmtmshr <s17t274@stu.kagawa-u.ac.jp>
2021-09-16 14:54:34 +0900

    mock-serverの追加

--- my_portfolio_front/portfolio/lib/fetchData.ts
+++ my_portfolio_front/portfolio/lib/fetchData.ts
@@ -3,82 +3,20 @@ import fetch from 'node-fetch'
 
 export const getAllPostsData = async () => {
 
-    // const res = await fetch(
-    //     new URL('https://jsonplaceholder.typicode.com/posts/')
-    // )
+    const res = await fetch(
+        new URL(`${process.env.API_URI}`)
+    )
 
-    // const posts = await res.json()
+    const posts = await res.json()
 
-    const posts = [{
-        id: 1,
-        title: 'dummy title 1',
-        body: 'dummy body 1',
-        created_at: "2021年10月2日"
-      },
-      {
-        id: 2,
-        title: 'dummy title 2',
-        body: 'dummy body 2',
-        created_at: "2021年10月3日"
-        },
-        {
-            id: 3,
-            title: 'dummy title 3',
-            body: 'dummy body 3',
-            created_at: "2021年10月4日"
-        },
-        {
-            id: 4,
-            title: 'dummy title 4',
-            body: 'dummy body 4',
-            created_at: "2021年10月5日"
-        },
-        {
-            id: 5,
-            title: 'dummy title 5',
-            body: 'dummy body 5',
-            created_at: "2021年10月5日"
-        },
-    ]
     return posts
 }
 
 export const getAllPostIds = async () => {
-    // const res = await fetch(
-    //     new URL('https://jsonplaceholder.typicode.com/posts/?_limit=10')
-    // )
-    // const posts = await res.json()
-    const posts = [{
-        id: 1,
-        title: 'dummy title 1',
-        body: 'dummy body 1',
-        created_at: "2021年10月2日"
-      },
-      {
-        id: 2,
-        title: 'dummy title 2',
-        body: 'dummy body 2',
-        created_at: "2021年10月3日"
-        },
-        {
-            id: 3,
-            title: 'dummy title 3',
-            body: 'dummy body 3',
-            created_at: "2021年10月4日"
-        },
-        {
-            id: 4,
-            title: 'dummy title 4',
-            body: 'dummy body 4',
-            created_at: "2021年10月5日"
-        },
-        {
-            id: 5,
-            title: 'dummy title 5',
-            body: 'dummy body 5',
-            created_at: "2021年10月5日"
-        },
-    ]
+    const res = await fetch(
+        new URL(`${process.env.API_URI}`)
+    )
+    const posts = await res.json()
     return posts.map((post) => {
         return {
             params: {
@@ -89,15 +27,9 @@ export const getAllPostIds = async () => {
 }
 
 export const getPostData = async (id: string) => {
-    // const res = await fetch(
-    //     new URL(`https://jsonplaceholder.typicode.com/posts/${id}`)
-    // )
-    // const post = await res.json()
-    const post = {
-        id: 5,
-        title: 'dummy title 5',
-        body: '# overviewaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaafaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n#### こんにちわ\n # overview\n# overview\n# overview\n# overview\n# overview\n# overview\n# overview\n# overview\n# overview\n',
-        created_at: "2021年10月5日"
-    }
+    const res = await fetch(
+        new URL(`${process.env.API_URI}${id}`)
+    )
+    const post = await res.json()
     return post
 }
